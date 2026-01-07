31a04f4795953cf4500a472ed1be3f7808aa8e7f
devnyoni <devnyoni@gmail.com>
2026-01-07 23:22:14 +0300

    Update inbox.js

--- commands/inbox.js
+++ commands/inbox.js
@@ -17,8 +17,8 @@ module.exports = {
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: "120363282833839832@newsletter",
-                        newsletterName: "Sᴀɴɴᴜ Mᴅ Mɪɴɪ Bᴏᴛ",
+                        newsletterJid: "120363399470975987@newsletter",
+                        newsletterName: "𝙼𝙾𝚁𝚃𝙰𝙻-𝙺𝙾𝙼𝙱𝙰𝚃-𝚇𝚁",
                         serverMessageId: 200
                     }
                 }
